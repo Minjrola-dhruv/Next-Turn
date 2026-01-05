7f58ffd1bc0ee965f22d0a16b5518c0f90005cc1
Dhruv <minjroladhruv3@gmail.com>
2026-01-05 20:56:04 +0530

    initial commit

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCZJDf4OAdFK0e7BDYLY5wYIeNR7ZhAfWI",
+//   authDomain: "nextturn-8217f.firebaseapp.com",
+//   projectId: "nextturn-8217f",
+//   storageBucket: "nextturn-8217f.firebasestorage.app",
+//   messagingSenderId: "884198002910",
+//   appId: "1:884198002910:web:6387c7d5816cc4acfc324f",
+//   measurementId: "G-B6B11FCNGL"
+// };
+/************* FIREBASE CONFIG *************/
+const firebaseConfig = {
+  apiKey: "AIzaSyCZJDf4OAdFK0e7BDYLY5wYIeNR7ZhAfWI",
+  authDomain: "nextturn-8217f.firebaseapp.com",
+  projectId: "nextturn-8217f",
+  storageBucket: "nextturn-8217f.appspot.com",
+  messagingSenderId: "884198002910",
+  appId: "1:884198002910:web:6387c7d5816cc4acfc324f",
+  databaseURL: "https://nextturn-8217f-default-rtdb.firebaseio.com"
+};
+
+// ❌ DO NOT initialize here
